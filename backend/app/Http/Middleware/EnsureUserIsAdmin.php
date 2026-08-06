adeab14bb9cab24bbf5a894805492e21a4c2518a
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-08-06 09:35:47 +0600

    Started Admin User works

---
+++ backend/app/Http/Middleware/EnsureUserIsAdmin.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureUserIsAdmin
+{
+    public function handle(
+        Request $request,
+        Closure $next
+    ): Response {
+        $user = $request->user();
+
+        if (
+            ! $user
+            || ! $user->hasRole('admin')
+        ) {
+            abort(
+                403,
+                'Administrator access is required.'
+            );
+        }
+
+        return $next($request);
+    }
+}
