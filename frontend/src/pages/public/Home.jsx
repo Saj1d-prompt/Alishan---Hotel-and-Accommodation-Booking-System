23c4b6d9732c8a64473f974ca989acfa924845cb
Saj1d-prompt <sajidulislam2225@gmail.com>
2026-08-09 09:59:36 +0600

    Removed why Choose section

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -1,6 +1,5 @@
 import HeroSection from "@/components/public/HeroSection";
 import LocationsSection from "@/components/public/LocationsSection";
-import WhyChooseSection from "@/components/public/WhyChooseSection";
 import ExperienceSection from "@/components/public/ExperienceSection";
 import CTASection from "@/components/public/CTASection";
 
@@ -9,7 +8,6 @@ const Home = () => {
     <>
       <HeroSection />
       <LocationsSection />
-      {/* <WhyChooseSection /> */}
       <ExperienceSection />
       <CTASection />
     </>
